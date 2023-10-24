2f32b35c6e6cd298187fb5429cde2615a0f0fc7a
Marquez Peralta Gael Aldair <22393137@utcancun.edu.mx>
2023-10-24 08:38:27 -0500

    Se modifico CSS

--- frontend/src/paginas/Inicio.js
+++ frontend/src/paginas/Inicio.js
@@ -6,28 +6,13 @@ import Footer from '../Componentes/Footer';
 function Inicio() {
     return (
         <>
-            <Header />
-            <div className='rectangle'>
-                <div className="circle">
-                    <div className='rectangle2'>
-                        <div className="circle2">
-                            <div className='rectangle3'>
-                                <div className="circle3">
-                                    <div className='rectangle4'>
-                                        <div className="circle4">
-
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <Footer />
-
-
-
+            <section className="blue">
+                <h1>¡Descubre el Universo en CosmiConnect!</h1>
+                <p>
+                Descubre el espacio como nunca antes. Horóscopos, planetas, estrellas y satélites, todo en un solo lugar. Únete a nosotros y despierta tu pasión por el universo.
+                </p>
+                <div className="curve" />
+            </section>
         </>
     );
 }
